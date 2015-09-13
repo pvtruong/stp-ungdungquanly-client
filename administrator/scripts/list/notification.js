7d76408e95ba6f4ecefff7113773d8c6a4014147
pvtruong <invncur@gmail.com>
2015-09-13 12:09:58 +0700

    Fixed error notification view

--- administrator/scripts/list/notification.js
+++ administrator/scripts/list/notification.js
@@ -1,10 +1,6 @@
 var notificationModule = new baseInput('notification','notification',["email_owner","email_sender","email_receiver","content","title"],'Thông báo',{
 	onView:function($scope,options){
-		var us = $scope.data.content.split('#');
-		if(us.length>1){
-				$scope.data.content = "#" + us[1];
-		}
-		
+		$scope.data.content = $scope.data.content.replace(server_url,"");
 		if(!$scope.data.read){
 			options.service.update(id_app,$scope.data._id,{_id:$scope.data._id,read:true}).success(function(r){
 				$scope.data.read = true;
