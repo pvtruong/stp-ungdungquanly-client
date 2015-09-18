814d834198ec39e19bf589e3713db3262af02eba
pvtruong <invncur@gmail.com>
2015-09-18 14:51:32 +0700

    Fix errors search in lists

--- administrator/configs.js
+++ administrator/configs.js
@@ -1,4 +1,5 @@
 var server_url="http://ungdungquanly.vn";
+//var server_url="http://localhost:8000";
 var modulesD=[
     //,'ngTouch'
 		       'ngRoute','ui.bootstrap','ngAnimate','luegg.directives','ngCookies','textAngular','ngTextcomplete','ui.slimscroll'
